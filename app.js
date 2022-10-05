6a66bb4698a19142a6724d8f2c29fa50634b5a48
olivia121283 <112666297+olivia121283@users.noreply.github.com>
2022-10-05 17:49:13 -0500

    Add files
    Cajero Automático.

---
+++ app.js
@@ -0,0 +1,61 @@
+const cuentas = [
+  { nombre: "Mali", saldo:200, password: '12345' },
+  { nombre: "Gera", saldo:290, password: '12345' },
+  { nombre: "Maui", saldo:67, password: '12345' }
+];
+
+function findUserByName(name, password) {
+  return cuentas.find(account => account.nombre === name)
+}
+
+/**
+ * Validate User and his password
+ * 
+ * @param {string} name - The user's name
+ * @param {string} password - The user's password
+ * @returns {boolean} A boolean value if user and password is valid
+ */
+function isUserIsValid(name, password) {
+  const account = findUserByName(name);
+  // early returns instead of if/else
+  if(!account) {
+    return false;
+  }
+
+  if(account.password !== password) {
+    return false;
+  }
+
+  return true;
+}
+
+function login(form){
+  const name = form.usuario.value;
+  const password = form.contraseña.value;
+
+  if(isUserIsValid(name, password)) {
+    const user = findUserByName(name, password)
+    localStorage.setItem('userLoggedIn', JSON.stringify(user))
+    window.location.replace('http://localhost:5500/Bienvenidos.html')
+    return;
+  }
+
+  alert("usuario o contraseña incorrecto")
+  form.usuario.value = "";
+  form.contraseña.value = "";
+
+}
+
+function checkIfUserIsLoggedIn() {
+  const userFromLS = localStorage.getItem('userLoggedIn');
+
+  if(userFromLS) {
+    window.location.replace("http://localhost:5500/Bienvenidos.html")
+  }
+
+}
+
+checkIfUserIsLoggedIn()
+
+
+
---
+++ bienvenido.js
@@ -0,0 +1,79 @@
+let userLoggedIn;
+
+function addAllEventListener() {
+  const button1 = document.getElementById('pay')
+
+  button1.addEventListener('click', paymentEvent);
+}
+
+function loadUserLoggedIn() {
+  const userFromLS = localStorage.getItem('userLoggedIn');
+
+  if(!userFromLS) {
+    window.location.replace("http://localhost:5500")
+    return;
+  }
+
+  userLoggedIn = JSON.parse(userFromLS)
+  loadUserData()
+}
+
+function loadUserData(){
+  const saldo = document.getElementById('saldo');
+  saldo.value = userLoggedIn.saldo;
+}
+
+function closeSession(){
+  localStorage.clear('userLoggedIn');
+  window.location.replace("http://localhost:5500")
+}
+
+function paymentEvent() {
+  let saldo = userLoggedIn.saldo;
+  let payment = parseInt(document.getElementById('abono').value)
+  // Regla del Negocio
+  if(saldo + payment > 990) {
+    alert ("Pasó el limite de su cuenta, no puede abonar esa cantidad")
+    return;
+  }
+    // Actualizamos el objeto usuario
+    userLoggedIn.saldo = saldo + payment;
+    // Actualizamos el usuario en el localstorage
+    updateUserInLocalStorage(userLoggedIn);
+    // Actualizamos la UI
+    document.getElementById('saldo').value = (saldo+payment)
+}
+
+function removeEvent() {
+  let saldo = userLoggedIn.saldo
+  let remove = parseInt(document.getElementById('abono').value)
+  // Regla del Negocio
+  if ( saldo - remove < 10) {
+    alert ("Su saldo dispoble no es suficiente, no puede tener menos de $10 en su cuenta")
+    return;
+  } 
+   // Actualizamos el objeto usuario
+   userLoggedIn.saldo = saldo - remove;
+   // Actualizamos el usuario en el localstorage
+   updateUserInLocalStorage(userLoggedIn);
+   // Actualizamos la UI
+   document.getElementById('saldo').value = (saldo-remove)
+}
+
+function updateUserInLocalStorage(user) {
+  localStorage.setItem('userLoggedIn', JSON.stringify(user))
+}
+
+function checkBalance(){
+  let saldo = userLoggedIn.saldo;
+  document.getElementById('saldo1').value = saldo
+  document.getElementById('saldo').value = " " 
+ }
+
+loadUserLoggedIn(); // Carga el usuario en sesion desde el localstorage
+addAllEventListener(); // Agrega todos los eventos que necesitaremos
+
+
+
+
+
